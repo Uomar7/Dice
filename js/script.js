f84254152fdf696c007b47aefe1426e8156a6d2b
Uomar <uomarearlie7@gmail.com>
2019-04-09 14:30:00 +0300

    disabling the classes function added

--- js/script.js
+++ js/script.js
@@ -47,12 +47,25 @@ Player.prototype.hold = function(){
 $("#hold-one").click(function() {
 
     document.getElementById("total1").innerHTML= playerOne.hold();
+    $("#p1").addClass("panelDisable");
+    $("#roller-one").addClass("panelDisable");
+    $("#hold-one").addClass("panelDisable");
+    $("#p2").removeClass("panelDisable");
+    $("#hold-two").removeClass("panelDisable");
+    $("#roller-two").removeClass("panelDisable");
     
 });
 
 $("#hold-two").click(function() {
 
+
     document.getElementById("total2").innerHTML= playerTwo.hold();
+    $("#p2").addClass("panelDisable");
+    $("#roller-two").addClass("panelDisable");
+    $("#hold-two").addClass("panelDisable");
+    $("#p1").removeClass("panelDisable");
+    $("#hold-one").removeClass("panelDisable");
+    $("#roller-one").removeClass("panelDisable");
     
 });
   playerOne.name = $("input#firstPlayer").val();
@@ -61,7 +74,8 @@ $("#hold-two").click(function() {
   document.getElementById("play1").innerHTML = playerOne.name;
   document.getElementById("play2").innerHTML = playerTwo.name;
    
-  
-      
+  $("#sub").click(function(){
+      $("#names").fadeOut();
+  })
     });
 });
